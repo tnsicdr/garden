088e0758f59beddb4f58d7fcec76bba323c94177
tensei <908739+tnsicdr@users.noreply.github.com>
2024-01-21 15:07:46 -0500

    chore: Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  site: "https://code.tnsi.me/garden",
+  integrations: [react()]
+});
